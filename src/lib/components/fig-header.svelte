105fbefb6e6ad88e0b15a63e3fbe2e232c0ee42e
nisarhassan12 <syednisarhassan12@gmail.com>
2022-03-31 23:29:40 +0500

    Gitpod Core Values.

---
+++ src/lib/components/fig-header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let image: { src: string; alt: string };
+  export let title: string;
+  export let text: string;
+  const { src, alt } = image;
+</script>
+
+<style lang="postcss">
+  h2 {
+    @apply mt-0 mb-macro;
+  }
+
+  p {
+    @apply my-0;
+  }
+</style>
+
+<div class="flex items-center mt-16 mb-x-small">
+  <img {src} {alt} class="h-24" />
+  <div class="pl-xx-small">
+    <h2>{title}</h2>
+    <p>{text}</p>
+  </div>
+</div>
--- src/lib/components/quote.svelte
+++ src/lib/components/quote.svelte
@@ -2,15 +2,20 @@
   let clazz = "";
   export { clazz as class };
   export let quote: string;
-  export let author: { name: string; jobTitle: string };
+  export let author: { name: string; jobTitle: string } = null;
+  export let type: "blog" | "" = "";
   import Card from "$lib/components/ui-library/card";
 
-  const { name, jobTitle } = author;
+  const { name, jobTitle } = author || {};
 </script>
 
 <style lang="postcss">
   [class|="kumquat"] {
     @apply absolute;
+  }
+
+  .quote {
+    @apply text-h5 mt-0 text-black;
   }
 
   .kumquat-left {
@@ -42,13 +47,15 @@
     />
     <img src="/svg/quotation-mark.svg" alt="Left Quotation Mark" />
     <div class="mt-x-small">
-      <p class="h3 text-black font-bold">
+      <p class="h3 text-black font-bold" class:quote={type === "blog"}>
         {@html quote}”
       </p>
-      <p class="mt-x-small">
-        <span class="font-semibold text-black">{name},</span>
-        {jobTitle}
-      </p>
+      {#if author}
+        <p class="mt-x-small">
+          <span class="font-semibold text-black">{name},</span>
+          {jobTitle}
+        </p>
+      {/if}
     </div>
 
     <img
