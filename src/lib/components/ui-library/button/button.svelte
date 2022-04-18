affc138141839fb746710c58f0caf3c16c707f0d
nisarhassan12 <syednisarhassan12@gmail.com>
2022-04-19 02:15:03 +0500

    make links dark mode compatible.

--- src/lib/components/ui-library/button/button.svelte
+++ src/lib/components/ui-library/button/button.svelte
@@ -14,7 +14,7 @@
     tertiary:
       "bg-important dark:bg-primary text-white dark:text-black hover:text-white hover:bg-less-important dark:hover:bg-quaternary",
     cta: "bg-sand-dark dark:bg-light-black dark:text-sand-dark hover:bg-tertiary hover:dark:bg-quaternary hover:dark:text-black focus:bg-tertiary",
-    gray: "bg-sand-dark dark:bg-light-black text-important hover:bg-tertiary focus:bg-tertiary hover:text-important focus:text-important dark:hover:bg-quaternary dark:hover:text-black",
+    gray: "bg-sand-dark dark:bg-light-black text-important dark:text-important hover:bg-tertiary focus:bg-tertiary hover:text-important focus:text-important dark:hover:bg-quaternary dark:hover:text-black",
     disabled: "pointer-events-none text-body bg-sand-dark",
     medium: "py-2 px-6 text-btn-small leading-4 rounded-xl",
     large: " py-3 px-8 text-p-medium leading-[1.25] min-w-[10rem] rounded-2xl",
