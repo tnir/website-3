5e0e89a0f1706f9ddb0581b1d3cdb2ee2b49e613
nisarhassan12 <syednisarhassan12@gmail.com>
2022-04-20 10:23:30 +0500

    refactor buttons.

--- src/lib/components/ui-library/button/button.svelte
+++ src/lib/components/ui-library/button/button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { buttonsClassMap } from "$lib/assets/buttons-class-map";
   import { current_component } from "svelte/internal";
   import { forwardEventsBuilder } from "../utils/eventforwarder";
   import type { ButtonSizes, ButtonVariations } from "./button";
@@ -8,20 +9,7 @@
   export let variant: ButtonVariations;
   export let disabled: boolean = false;
 
-  const classMap = {
-    primary: "bg-primary text-important dark:text-black hover:bg-quaternary",
-    secondary:
-      "bg-salmon hover:bg-salmon-hover dark:text-black focus:bg-salmon-hover",
-    tertiary:
-      "bg-important dark:bg-primary text-white dark:text-black hover:text-white hover:bg-less-important dark:hover:bg-quaternary",
-    cta: "bg-sand-dark dark:bg-light-black dark:text-sand-dark hover:bg-tertiary hover:dark:bg-quaternary hover:dark:text-black focus:bg-tertiary",
-    gray: "bg-sand-dark dark:bg-light-black text-important dark:text-important hover:bg-tertiary focus:bg-tertiary hover:text-important focus:text-important dark:hover:bg-quaternary dark:hover:text-black",
-    disabled: "pointer-events-none text-body bg-sand-dark",
-    medium: "py-2 px-6 text-btn-small leading-4 rounded-xl",
-    large: " py-3 px-8 text-p-medium leading-[1.25] min-w-[10rem] rounded-2xl",
-    small: "py-1 px-4 text-p-xsmall rounded-xl",
-    unstyled: "",
-  };
+  const classMap = buttonsClassMap;
 
   const forwardEvents = forwardEventsBuilder(current_component);
 </script>
