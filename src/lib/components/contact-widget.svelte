69083192cd0c05354a753532a100aa81d56fcf45
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2022-08-15 14:49:08 +0500

    improve the contact widget. (#2568)

--- src/lib/components/contact-widget.svelte
+++ src/lib/components/contact-widget.svelte
@@ -17,27 +17,42 @@
   const buttons: {
     text: string;
     href: string;
-    icon: any;
+    icon: {
+      component: any;
+      size?: any;
+    };
   }[] = [
     {
       text: "Schedule a demo",
       href: "/contact/sales?get-a-demo",
-      icon: Play,
+      icon: {
+        component: Play,
+        size: "h-[12.94px] w-[10.78px]",
+      },
     },
     {
       text: "Contact Sales",
       href: "/contact/sales",
-      icon: Headphones,
+      icon: {
+        component: Headphones,
+        size: "h-[14.38px] w-[20.48px]",
+      },
     },
     {
       text: "Get support",
       href: "/contact/support",
-      icon: Chat,
+      icon: {
+        component: Chat,
+        size: "h-[13.33px] w-[16.74px]",
+      },
     },
     {
       text: "View documentation",
       href: "/docs",
-      icon: File,
+      icon: {
+        component: File,
+        size: "h-[17.28px] w-[14.78px]",
+      },
     },
   ];
 
@@ -76,24 +91,6 @@
     @apply bottom-[105px] md:bottom-2;
   }
 
-  .links {
-    @apply relative;
-
-    .before,
-    .after {
-      @apply absolute bottom-0 right-6 h-6 w-6;
-    }
-
-    .before {
-      @apply bg-sand-dark dark:bg-card h-[1px] w-[31px] right-[20px];
-    }
-
-    .after {
-      @apply -z-10;
-      transform: translateY(47%) rotate(45deg);
-    }
-  }
-
   .icon-wrapper {
     &::before {
       content: "";
@@ -121,22 +118,25 @@
 </style>
 
 <div
-  class="fixed bottom-2 right-2 flex flex-col items-end z-50 parent"
+  class="fixed bottom-2 right-2 sm:right-4 flex flex-col items-end z-50 parent"
   data-analytics={`{"context":"contact_widget"}`}
 >
   {#if areButtonsShown}
     <div
-      in:fade={{ duration: 600 }}
-      out:fade={{ duration: 300 }}
+      in:fade={{ duration: 400 }}
+      out:fade={{ duration: 100 }}
       bind:this={linksWrapper}
-      class="stroked stroked-sand flex flex-col rounded-2xl mb-5 sm:mb-xx-small links p-micro pt-x-small shadow-light
+      class="stroked stroked-sand flex flex-col rounded-2xl mb-3 p-micro pt-x-small shadow-light
       "
     >
-      <div data-analytics={`{"label":"Close Contact Widget"}`}>
+      <div
+        data-analytics={`{"label":"Close Contact Widget"}`}
+        class="absolute -top-2 -right-2"
+      >
         <button
           on:click={() => (areButtonsShown = false)}
           aria-label="Close the menu"
-          class="absolute h-12 w-12 -top-1 -right-1 flex items-center justify-center rounded-full"
+          class="h-12 w-12 flex items-center justify-center rounded-full"
           on:mouseenter={() => {
             isButtonActive = true;
           }}
@@ -150,28 +150,32 @@
             isButtonActive = false;
           }}
         >
-          <Close class="h-[18px] w-[18px]" active={isButtonActive} />
+          <Close class="h-2.5 w-2.5" active={isButtonActive} />
         </button>
       </div>
       <div class="before" />
-      <div class="space-y-macro max-w-[214px]">
+      <div class="space-y-macro max-w-[195px]">
         {#each buttons as { href, text, icon }}
           <LinkButton
             {href}
             variant="white"
             textAlign="left"
             class="inline-flex items-center group"
+            style="padding: 0.25rem var(--micro)"
           >
-            <svelte:component
-              this={icon}
-              class="h-4 w-4 mr-3 filter grayscale group-hover:grayscale-0 transition-all duration-200"
+            <div
               slot="image"
-            />
+              class="flex items-center justify-center h-6 w-6 mr-2"
+            >
+              <svelte:component
+                this={icon.component}
+                class="{icon.size} filter grayscale group-hover:grayscale-0 transition-all duration-200"
+              />
+            </div>
             {text}
           </LinkButton>
         {/each}
       </div>
-      <div class="stroked stroked-sand after" />
     </div>
   {/if}
 
@@ -179,7 +183,7 @@
     <div data-analytics={`{"label":"Hide/Show Contact Widget"}`}>
       <button
         in:fade={{ duration: 200 }}
-        class="stroked flex group justify-center items-center bg-card h-12 w-12 sm:h-14 sm:w-14 rounded-full shadow-normal"
+        class="stroked flex group justify-center items-center bg-card h-12 w-12 rounded-full shadow-normal"
         on:click={() => {
           areButtonsShown = !areButtonsShown;
         }}
@@ -187,7 +191,7 @@
         <div class="icon-wrapper" bind:this={iconWrapper}>
           <svelte:component
             this={Chat}
-            class="h-6 w-6 sm:h-8 sm:w-8 transition-all duration-200"
+            class="h-6 w-6 transition-all duration-200"
             id="toggle-button"
           />
         </div>
--- src/lib/components/svgs/icons/close.svelte
+++ src/lib/components/svgs/icons/close.svelte
@@ -9,25 +9,16 @@
 </style>
 
 <svg
-  viewBox="0 0 25 24"
+  viewBox="0 0 13 13"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
-  {...$$props}
+  {...$$restProps}
 >
   <path
-    d="M0.689819 12.0002C0.689819 5.37283 6.0624 0.000244141 12.6898 0.000244141C19.3172 0.000244141 24.6898 5.37283 24.6898 12.0002C24.6898 18.6277 19.3172 24.0002 12.6898 24.0002C6.0624 24.0002 0.689819 18.6277 0.689819 12.0002Z"
-    class={active ? "fill-card dark:fill-sand-dark" : "fill-bg dark:fill-sub"}
-  />
-  <path
-    d="M9.18982 8.00024L17.1898 16.0002"
-    class="stroke-sub dark:stroke-card"
-    stroke-width="2"
+    d="M1.48975 1.49609L11.4976 11.5039M11.4976 1.49609L1.48975 11.5039"
+    class={active ? "stroke-important" : "stroke-[#999795] dark:stroke-sub"}
+    stroke-width="1.66797"
     stroke-linecap="round"
-  />
-  <path
-    d="M17.1898 8.00024L9.18982 16.0002"
-    class="stroke-sub dark:stroke-card"
-    stroke-width="2"
-    stroke-linecap="round"
+    stroke-linejoin="round"
   />
 </svg>
